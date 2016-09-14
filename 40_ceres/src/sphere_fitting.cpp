4ef8e68564433f6f70c052f9340e29faf67102cc
andreucm <acoromin@iri.upc.edu>
2016-09-14 14:01:49 +0200

    Adding noise to data. Workng copy. Not yet built.

--- 40_ceres/src/sphere_fitting.cpp
+++ 40_ceres/src/sphere_fitting.cpp
@@ -31,8 +31,9 @@ int main(int argc, char** argv)
     //program arguments. Num points, center of the sphere and radius
     unsigned int np = 100; 
     Eigen::Vector3d center << 5,-6,7; 
-    double rr = 3.7; 
-    noise_stddev = 0.2; 
+    double radius = 3.7; 
+    double noise_stddev = 0.2; 
+    double outlier_ratio = 0.1; 
     
     //required inits for ceres
     google::InitGoogleLogging(argv[0]);
@@ -40,25 +41,49 @@ int main(int argc, char** argv)
     // Declare the ceres problem.
     ceres::Problem problem;
 
-    //generate spherical points with noise and outliers
+//************** GERNERATE MEASUREMENTS *************************
+    
+    //generate points on the sphere surface
     std::default_random_engine rnd_gen;
     std::uniform_real_distribution<double> rnd_uniform(0.0,1.0);
-    std::normal_distribution<double> rnd_normal(5.0,2.0);    
+    std::normal_distribution<double> rnd_normal(0.,noise_stddev);    
     Eigen::MatrixXd points;
     points.resize(3,np);
     Eigen::Quaterniond q_rnd;
     Eigen::Matrix3d m_rnd; 
-    Eigen::Vector3d v_rnd; 
+    Eigen::Vector3d v_rnd;
+    Eigen::Vector3d noise; 
     for (unsigned int ii=0; ii<np; ii++)
     {
         q_rnd.vec() << rnd_uniform(rnd_gen), rnd_uniform(rnd_gen), rnd_uniform(rnd_gen); 
-        q_rnd.w() = sqrt(1 -  ); 
-        points.block<3,1>(0,ii) = center + 
+        q_rnd.w() = sqrt( 1 - q_rnd.x()*q_rnd.x() + q_rnd.y()*q_rnd.y() + q_rnd.z()*q_rnd.z() ); 
+        m_rnd = q_rnd.toRotationMatrix(); 
+        v_rnd = m_rnd.block<3,1>(0,0); //first column is the X vector of the frame
+        points.block<3,1>(0,ii) = center + radius*v_rnd; 
     }
     
-    //add noise to points
+    //add normal noise to points
+    for (unsigned int ii=0; ii<np; ii++)
+    {
+        noise << rnd_normal(rnd_gen), rnd_normal(rnd_gen), rnd_normal(rnd_gen); 
+        points.block<3,1>(0,ii) = points.block<3,1>(0,ii) + noise; 
+    }
 
-    //create measurements and add constraints to th problem
+    //create outliers
+    for (unsigned int ii=0; ii<np; ii++)
+    {
+        if ( rnd_uniform(rnd_gen) < outlier_ratio )
+        {
+            //
+        }
+    }
+    
+    
+//***************************************************************
+
+//*************** CERES PROBLEM *********************************
+
+    //create measurements and add constraints to the problem
 
     //solve: estimate sphere parameters
 
