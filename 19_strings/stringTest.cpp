2cdf7fc76dc0c0928e56abec040662b521fd0214
andreucm <andreu@beta-robots.com>
2019-10-31 15:56:01 +0100

    Added std::string::insert() example

--- 19_strings/stringTest.cpp
+++ 19_strings/stringTest.cpp
@@ -27,10 +27,10 @@
 int main()
 {
 	//playing with strings
-	std::string aa, bb, cc; 
+	std::string aa, bb, cc;
 	char buf[50];
       int aaInt;
-      
+
 	aa="Hello";
 	bb="World!";
 	aa.append(" ");
@@ -44,36 +44,44 @@ int main()
 	buf[0] = 'a';buf[1] = 'b';buf[2] = '\0';
 	aa.append(buf);
 	std::cout << aa << std::endl;
-	
+
 	std::string myStr = "hola";
-	
+
 	if (myStr.compare("") != 0) std::cout << "myStr is not void" << std::endl;
 	else std::cout << "myStr is void" << std::endl;
-	
+
 	//string exportCmnd = "export DISPLAY=':0.0'";
 	//string exportCmnd = "export ZZZ=':0.0'";
 	//system(exportCmnd.c_str());
-      
+
       //string to integer
       aa = "123000";
       //bool rtV = String2Int(aa,aaInt);
       //std::cout << rtV << "; " << aaInt << std::endl;
-      
-      
+
+
       std::string s1("hola");
       std::string s2("hola");
-      
+
       std::cout << "s1.compare(s2): " << s1.compare(s2) << std::endl;
-      std::cout << "s2.compare(s1): " << s2.compare(s1) << std::endl;
-      
+
+	  std::cout << "s2.compare(s1): " << s2.compare(s1) << std::endl;
       std::string s3("jpñs");
       std::cout << "s1.compare(s3): " << s1.compare(s3) << std::endl;
       std::cout << "s3.compare(s1): " << s3.compare(s1) << std::endl;
-      
+
       s1 = "<orderItem>";
       s2 = "<orderItem>";
       std::cout << "s1.compare(s2): " << s1.compare("<orderItem>") << std::endl;
       std::cout << "s2.compare(s1): " << s2.compare(s1) << std::endl;
-      	
+
+	  std::string str_1("param1");
+	  str_1.insert(0,"namespace/");
+	  std::cout << "str_1: " << str_1 << std::endl;
+
+	  std::string str_2("namespace");
+	  std::cout << "str_2+std::string(\"/param1\"): " << str_2+std::string("/param1") << std::endl;
+	  std::cout << "str_2.append(\"/param1\"): " << str_2.append("/param1") << std::endl;
+
 	return 0;
 }
