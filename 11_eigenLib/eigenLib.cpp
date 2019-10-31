25da6f939f4e60a671c189a1c39d90ebe3b20e7a
andreucm <andreu@beta-robots.com>
2019-10-31 15:54:41 +0100

    Minor updates to clean spaces

--- 11_eigenLib/eigenLib.cpp
+++ 11_eigenLib/eigenLib.cpp
@@ -1,6 +1,6 @@
 /**helloWorld.cpp*/
 
-//Std 
+//Std
 #include <iostream>
 #include <time.h>
 #include <sys/time.h>
@@ -11,7 +11,7 @@
 #include <eigen3/Eigen/Dense>
 #include <eigen3/Eigen/Geometry>
 
-// import most common Eigen types 
+// import most common Eigen types
 using namespace std;
 using namespace Eigen;
 
@@ -29,15 +29,15 @@ int main(int argc, char *argv[])
     double t1, t2;
     MatrixXi bigMatrix, bigMatrixResult;
     int mSize = 1500;
-    MatrixXd md; 
-    
+    MatrixXd md;
+
     //get user input
     int user_choice = atoi(argv[1]);
-    
+
     //main switch
     switch(user_choice)
     {
-        case 1: 
+        case 1:
             std::cout << std::endl << "Test1: set and access" << std::endl;
             cout << mz << endl;
             m3 << 1, 2, -3, 4, -5, 6, -7, 8, 9;
@@ -48,7 +48,7 @@ int main(int argc, char *argv[])
             cout << "m3(1,:) = " << m3.block<1,3>(1,0) << " = "<< m3.block(1,0,1,3) << endl;
             cout << "m3(:,1) = \n" << m3.block<3,1>(0,1) << endl;
             break;
-            
+
         case 2:
             std::cout << std::endl << "Test2: operations" << std::endl;
             m3 << 1, 2, -3, 4, -5, 6, -7, 8, 9;
@@ -69,22 +69,22 @@ int main(int argc, char *argv[])
             mp3 = new Matrix3f();
             *mp3 << 11, 22, -33, 44, -55, 66, -77, 88, 99;
             cout << "(*mp3)(0,0)=" << (*mp3)(0,0) << endl;
-            uu = vv+3*Vector3f::Ones(); 
+            uu = vv+3*Vector3f::Ones();
             break;
-            
+
         case 3:
             std::cout << std::endl << "Test3: matrixXf" << std::endl;
-            
-            gettimeofday(&ts, NULL); 
-            t1 = (double)(ts.tv_sec + ts.tv_usec/1e6);  
+
+            gettimeofday(&ts, NULL);
+            t1 = (double)(ts.tv_sec + ts.tv_usec/1e6);
             bigMatrix.resize(mSize,mSize);
-            gettimeofday(&ts, NULL); 
-            t2 = (double)(ts.tv_sec + ts.tv_usec/1e6);  
+            gettimeofday(&ts, NULL);
+            t2 = (double)(ts.tv_sec + ts.tv_usec/1e6);
             cout << "Time resizing: " << t2-t1 << endl;
 
             int ii,jj;
-            gettimeofday(&ts, NULL); 
-            t1 = (double)(ts.tv_sec + ts.tv_usec/1e6);      
+            gettimeofday(&ts, NULL);
+            t1 = (double)(ts.tv_sec + ts.tv_usec/1e6);
             for (ii=0; ii<bigMatrix.rows(); ii++)
             {
                 for (jj=0; jj<bigMatrix.cols(); jj++)
@@ -92,33 +92,45 @@ int main(int argc, char *argv[])
                     bigMatrix(ii,jj) = (ii+jj);
                 }
             }
-            gettimeofday(&ts, NULL); 
-            t2 = (double)(ts.tv_sec + ts.tv_usec/1e6);  
+            gettimeofday(&ts, NULL);
+            t2 = (double)(ts.tv_sec + ts.tv_usec/1e6);
             cout << "Time filling the matrix: " << t2-t1 << endl;
-            
+
             bigMatrixResult.resize(mSize, mSize);
-            gettimeofday(&ts, NULL); 
-            t1 = (double)(ts.tv_sec + ts.tv_usec/1e6);  
+            gettimeofday(&ts, NULL);
+            t1 = (double)(ts.tv_sec + ts.tv_usec/1e6);
             bigMatrixResult = bigMatrix*bigMatrix;
-            gettimeofday(&ts, NULL); 
-            t2 = (double)(ts.tv_sec + ts.tv_usec/1e6);  
+            gettimeofday(&ts, NULL);
+            t2 = (double)(ts.tv_sec + ts.tv_usec/1e6);
             cout << "Time for matrix product: " << t2-t1 << endl;
             break;
-            
-        case 4: 
+
+        case 4:
             md.setZero(1,3);
             cout << md << endl;
-            break; 
-        
-        case 5: 
+            break;
+
+        case 5:
             uu << 1, 0, -4.2;
-            std::cout << uu.block<2,1>(1,0) << std::endl; 
+            std::cout << uu.block<2,1>(1,0) << std::endl;
+			break;
+
+		case 6:
+		{
+			Matrix3d AA, BB;
+			AA = Eigen::Matrix3d::Identity();
+			BB = Eigen::Matrix3d::Zero();
+			MatrixXd CC(3,6);
+			CC << AA,BB;
+			std::cout << "CC: " << std::endl << CC << std::endl;
+		}
+			break;
 
         default:
             std::cout << std::endl << "Unknown Test Id. EXIT" << std::endl;
-            break;            
+            break;
     }
-    
+
     return 0;
 }
 //results:
